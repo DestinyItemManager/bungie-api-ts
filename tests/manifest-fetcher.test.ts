420e4e971c8322ac904331956f59351bf609c886
nev <gh@f-m.fm>
2021-11-23 00:42:50 -0800

    test fixes

--- tests/manifest-fetcher.test.ts
+++ tests/manifest-fetcher.test.ts
@@ -1,80 +1,8 @@
-import {
-  getDestinyManifest,
-  getDestinyEntityDefinition,
-  searchDestinyPlayer,
-  getLinkedProfiles,
-  getProfile,
-  getCharacter,
-  getClanWeeklyRewardState,
-  getItem,
-  getVendors,
-  getVendor,
-  getPublicVendors,
-  getCollectibleNodeDetails,
-  transferItem,
-  pullFromPostmaster,
-  equipItem,
-  equipItems,
-  setItemLockState,
-  setQuestTrackedState,
-  insertSocketPlug,
-  getPostGameCarnageReport,
-  reportOffensivePostGameCarnageReportPlayer,
-  getHistoricalStatsDefinition,
-  getClanLeaderboards,
-  getClanAggregateStats,
-  getLeaderboards,
-  getLeaderboardsForCharacter,
-  searchDestinyEntities,
-  getHistoricalStats,
-  getHistoricalStatsForAccount,
-  getActivityHistory,
-  getUniqueWeaponHistory,
-  getDestinyAggregateActivityStats,
-  getPublicMilestoneContent,
-  getPublicMilestones,
-  awaInitializeRequest,
-  awaProvideAuthorizationResult,
-  awaGetActionToken,
-} from '../generated-src/destiny2/api';
+import { getDestinyManifest } from '../generated-src/destiny2/api';
 import { getDestinyManifestSlice } from '../generated-src/destiny2/manifest';
-import { HttpClient, HttpClientConfig } from '../generated-src/http';
-import {
-  DestinyComponentType,
-  DestinyInventoryItemDefinition,
-  DestinyLocationDefinition,
-} from '../generated-src/destiny2/interfaces';
+import { HttpClientConfig } from '../generated-src/http';
+
 import fetch from 'node-fetch';
-
-// test('should throw an error if there is no room in the destination', async () => {
-//   expect.assertions(1);
-//   await expect(async () => {
-//     (await pullFromPostmaster(pretendHttpClient(errors.DestinyNoRoomInDestination), {
-//       characterId: '1234658790',
-//       membershipType: 3,
-//       itemId: '0987654321',
-//       itemReferenceHash: 45674576,
-//       stackSize: 7,
-//     })) as any;
-//   }).rejects.toMatchInlineSnapshot(
-//     `[BungieError: There are no item slots available to transfer this item.]`
-//   );
-// });
-
-// test('should throw an error if API is down for maintenance', async () => {
-//   expect.assertions(1);
-//   await expect(async () => {
-//     (await pullFromPostmaster(pretendHttpClient(errors.SystemDisabled), {
-//       characterId: '1234658790',
-//       membershipType: 3,
-//       itemId: '0987654321',
-//       itemReferenceHash: 45674576,
-//       stackSize: 7,
-//     })) as any;
-//   }).rejects.toMatchInlineSnapshot(
-//     `[BungieError: This system is temporarily disabled for maintenance.]`
-//   );
-// });
 
 async function httpClient(config: HttpClientConfig) {
   return fetch(config.url, config)
